3650f73c0f74f16c21bd44616d740b6a6a48151c
Tim Schraepen <tim.schraepen@sfpd.fgov.be>
2025-12-01 17:46:20 +0100

    2025 day 1 solution with Hektor on the train to Leuven

--- src/test/kotlin/be/swsb/aoc2024/day1/Day1Test.kt
+++ src/test/kotlin/be/swsb/aoc2024/day1/Day1Test.kt
@@ -3,6 +3,7 @@ package be.swsb.aoc2024.day1
 import be.swsb.aoc2024.util.readFile
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kotlin.time.measureTime
 
 
 class Day1Test : FunSpec({
@@ -21,10 +22,24 @@ class Day1Test : FunSpec({
             L82
         """.trimIndent()
 
-        solve(input) shouldBe 3
-        solve(readFile("day1/input.txt")) shouldBe 1180
+        measureTime { solve(input) shouldBe 3 }.also { println(it) }
+        measureTime { solve(readFile("day1/input.txt")) shouldBe 1180 }.also { println(it) }
+
+        measureTime { solveV2(input) shouldBe 3 }.also { println(it) }
+        measureTime { solveV2(readFile("day1/input.txt")) shouldBe 1180 }.also { println(it) }
     }
 })
+
+fun solveV2(input: String): Int {
+    var amountOfTimesAt0 = 0
+    val dial = MutableDial(50)
+    for (line in input.lines()) {
+        if (line.startsWith("L")) dial.left(line.drop(1).toInt())
+        else dial.right(line.drop(1).toInt())
+        if (dial.arrow == 0) amountOfTimesAt0++
+    }
+    return amountOfTimesAt0
+}
 
 fun solve(input: String): Int {
     val dial = input.lines().fold(Dial(arrow = 50)) { acc, instr ->
@@ -34,6 +49,17 @@ fun solve(input: String): Int {
             .let { dial -> if (dial.arrow == 0) dial.copy(amountOfTimesAt0 = dial.amountOfTimesAt0+1) else dial }
     }
     return dial.amountOfTimesAt0
+}
+
+class MutableDial(arrow: Int) {
+    var arrow: Int = arrow
+        private set
+    fun left(distance: Int) {
+        arrow = (arrow - distance).mod(100)
+    }
+    fun right(distance: Int) {
+        arrow = (arrow + distance).mod(100)
+    }
 }
 
 data class Dial(val arrow: Int, val amountOfTimesAt0: Int = 0) {
